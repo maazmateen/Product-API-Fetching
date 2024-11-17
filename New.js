200de48afc1ee1b801a277b90427e84db4cf6f72
maazmateen <maazmateen75@gmail.com>
2024-11-18 03:02:18 +0500

    font awesome added in new.js

--- New.js
+++ New.js
@@ -14,15 +14,20 @@ async function GetData(params) {
     console.log(json);
     
      div2.innerHTML =`
-        <div class="card" style="width: 18rem;">
-        <img src="${json.thumbnail}" class="card-img-top" alt="...">
+        <div class="card" style="width: 25rem;">
+        <img src="${json.thumbnail}" class="card-img-top" alt="Products">
         <hr />
         <div class="card-body">
         <h6 class="Item-category">${json.category}</h6>
-          <h5 class="card-title">${json.title}</h5>
-          <p class="card-discription">${json.description}</p>
-          <h6>InStock : ${json.stock}</h6>
-         <h6>Price : ${json.price} PKR</h6>
+        <h5 class="card-title">${json.title}</h5>
+        <P>More About This Product</P><div class="Icon-Box">
+        <i class="fa-solid fa-arrow-turn-down"></i></div>
+          <p class="card-discription2">${json.description}</p>
+          <h6>Warranty : ${json.warrantyInformation}</h6>
+          <h6>InStock : ${json.stock} Piecies</h6>
+         <h6>Price : ${json.price}$</h6>
+         <h6>Discount : ${json.discountPercentage}%</h6>
+         <h6>Shipping Information : ${json.shippingInformation}</h6>
           
           
         </div>
